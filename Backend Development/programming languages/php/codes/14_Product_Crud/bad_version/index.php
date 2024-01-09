5649169d3d67aacf2c0dbbeaaab98aebaa16e837
Abdullah AlHabal <dbnkalhbalb@gmail.com>
2024-01-09 11:54:48 +0300

    Organization The Structure of the Repository

--- Backend Development/programming languages/php/codes/14_Product_Crud/bad_version/index.php
+++ Backend Development/programming languages/php/codes/14_Product_Crud/bad_version/index.php
@@ -4,6 +4,7 @@
 # 1- create an instance of the PDO class : $pdo = new PDO('dsn sting : defines the connection string to the database mysql:host=localhost;port:3306 the default port of mysql ;the database name dbname=products_crud' , 'the user and in all cases is root', 'the password and for windows usrs is empty password');
 # we can see the port from Xampp control pannel 
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud','root','');
+# ('mysql:host=;port=;dbname=' , 'root' , '') .
 # using this line of code , we actullay already establish connection to the database , but we want to tell $pdo what to do if the connection to the database not successfully done. 
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
@@ -18,6 +19,14 @@ echo "<h1> We are herr ! </h1>";
 var_dump($products);
 echo '</pre>';
 
+/*
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud','root','');
+// mysql: hots=localhost;port=; dbnam=
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+ */
 ?>
 
 <!doctype html>
@@ -45,34 +54,32 @@ echo '</pre>';
 
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Image</th>
+              <th scope="col">Title</th>
+              <th scope="col">Price</th>
+              <th scope="col">Create Date</th>
+              <th scope="col">Action</th>
             </tr>
 
           </thead>
 
           <tbody>
 
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-
+            <?php foreach($products as $i => $product):?>
+              <tr>
+                <th scope='row'> <?php echo $i ?> </th>
+                <td><?php echo $product['image'] ?></td>
+                <td><?php echo $product['title'] ?></td>
+                <td><?php echo $product['price'] ?></td>
+                <td><?php echo $product['create_date'] ?></td>
+                <td>
+                  <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
+                  <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+                </td>
+              </tr>
+            <?php endforeach ?>
+            
+            
           </tbody>
 
         </table>
